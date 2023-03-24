3ad6291d8cf8c721b6a76f20a15f3350f3eaf09f
tle877 <tle877@csu.fullerton.edu>
2023-03-23 21:31:05 -0700

    Update typo

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-
 
 let lengthResult = document.getElementById("length-result")
 let volumeResult = document.getElementById("volume-result")
